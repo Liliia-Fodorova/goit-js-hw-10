ba6b615dbfc091e69a0c02e7e7dad22fb55009a0
Liliia Fodorova <nevskay22@gmail.com>
2026-05-15 23:25:09 +0200

    commit bibliotic and timer,js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,40 @@
 console.log('TIMER');
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startBtn = document.querySelector('[data-start]');
+const input = document.querySelector('#datetime-picker');
+
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let selectedDate = null;
+let timerId = null;
+
+startBtn.disable = true;
+
+flatpickr(input, {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+
+    onClose(selectedDates) {
+        selectedDate = selectedDates[0];
+        if (selectedDate <= new Date()) {
+            startBtn.disable = true;
+            iziToast.error({
+                maessage: "Please choose a date in the future",
+                position: 'topRight',
+            });
+        } else {
+            startBtn.disable = false;
+        }
+    },
+});
+
